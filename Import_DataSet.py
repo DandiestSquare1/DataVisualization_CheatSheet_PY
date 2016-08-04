3815999b6c2a9079e0498661bc1e168094265a03
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-04 16:41:02 +0530

    Importing Modules and Data

---
+++ Import_DataSet.py
@@ -0,0 +1,8 @@
+"""
+- Importing Modules : matplotlib, pandas
+- Importing Dataset : CricSheets.yaml
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_excel('...\PycharmProjects\CricSheet.xlsx', 'Sheet1')
