280c2d85755ef80f86d9b567aad5ae2494e54571
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-10 19:16:25 +0530

    PieChart

--- PieChart.py
+++ PieChart.py
@@ -2,10 +2,10 @@
 PieChart -- Using matplotlib / pandas
 """
 
-var = df.groupBy(['Players'])
+var = df.groupBy(['Players']).sum().stack()
 temp = var.unstack()
 type(temp)
-x_list = temp['ABC']
+x_list = temp['Runs']
 lable_list = temp.index
 pyplot.axis('Values')
 plt.pie(x_list, labels = label_list, autopct = "%1.1f%")
