a9b3343403910cb5a3bccb88f9dd3185ea002b0a
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-10 19:19:10 +0530

    BarChart

--- BarChart.py
+++ BarChart.py
@@ -2,3 +2,11 @@
 BarChart -- Using matplotlib / pandas
 """
 
+var = df.groupby(['Country']).Runs.sum()
+fig = plt.figure()
+ax = fig.subplot(1,1,1)
+ax.set_xlabel('Country')
+ax.set_ylabel('Runs')
+plt.title('Highest Run Getters - Country Wise')
+
+plt.show()
